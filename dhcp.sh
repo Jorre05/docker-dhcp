d939302f726a1c87d0b1dde3db2d2dc6cfc9d821
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-01-12 18:40:37 +0100

    Update dhcp.sh

--- dhcp.sh
+++ dhcp.sh
@@ -11,7 +11,7 @@ git clone ${GITHUB_CONFIG_REPO} ${GITHUB_CLONE_DIR}
 
 echo "kopie naar config"
 cp -frp ${GITHUB_CLONE_DIR}/dhcp/* /config
-sed s/#LISTEN_ON#/${LISTEN_ON}/g /config/kea-dhcp4.conf -i
+sed "s/#LISTEN_ON#/${LISTEN_ON}/g" /config/kea-dhcp4.conf -i
 sed "s/#DNS_SERVERS#/${DNS_SERVERS}/g" /config/kea-dhcp4.conf -i
 
 /usr/sbin/kea-dhcp4 -c /config/kea-dhcp4.conf &
